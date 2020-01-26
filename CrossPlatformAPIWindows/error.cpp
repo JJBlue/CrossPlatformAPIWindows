999d683f8b30d0979344f0557016c7c3512c6801
JJBlue <lieb.julian@gmx.de>
2020-01-26 13:04:16 +0100

    added error for debugging

--- CrossPlatformAPIWindows/error.cpp
+++ CrossPlatformAPIWindows/error.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-static void printError() {
+void printError() {
 	wchar_t buf[256];
 	FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), buf, (sizeof(buf) / sizeof(wchar_t)), NULL);
 	std::wcout << buf << std::endl;
